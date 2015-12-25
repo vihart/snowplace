b50a8f7d560986353fe8c0bee74de76040c5969a
Vi Hart <vi@vihart.com>
2015-12-24 18:20:49 -0800

    decorated trees, present finding game

--- js/snowplace.js
+++ js/snowplace.js
@@ -1,5 +1,6 @@
 var phi = 1.618033988749894848;
 var pi = 3.14159265359;
+var tau = 6.28318530717958;
 
 // Setup three.js WebGL renderer
 var renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -41,7 +42,7 @@ for (var i = 0; i < plane2.geometry.vertices.length; i++){
 
 //snowground
 var planeGeometry = new THREE.PlaneGeometry( 100, 100, 50, 50 );
-var planeMaterial = new THREE.MeshLambertMaterial( {color: 0xffffff, side: THREE.DoubleSide, wireframe:false} );
+var planeMaterial = new THREE.MeshPhongMaterial( {color: 0xffffff, side: THREE.DoubleSide, wireframe:false} );
 var plane = new THREE.Mesh( planeGeometry, planeMaterial );
 plane.rotation.x = -pi/2;
 everything.add( plane );
@@ -93,7 +94,7 @@ everything.add( plane3 );
   everything.add(particleSystem);
 
 //lights    
-var light = new THREE.PointLight( 0xffffff, 1, 200);
+var light = new THREE.PointLight( 0xffffff, 0.7, 100);
 light.position.set( -10,25,-2);
 light.castShadow = true;
 everything.add( light );
@@ -120,27 +121,111 @@ for (var i = 0; i < treeNumber; i++){
 }
 
 var pine = [];
-var pineNumber = 2;
+var pineNumber = 4;
 var branch = [];
+var starlight = [];
+var treeLight = [];
 for (var i = 0; i < pineNumber; i++){
-  var branchNumber = Math.random()*10 + 10;
+  var branchNumber = Math.random()*20 + 30;
   pine[i] = new THREE.Object3D();
+  var treeTrunkWidth = 0.08 + Math.random()/20;
+  var treeTrunkHeight = 0.25 + Math.random()/2;
+  var treeTrunk = new THREE.Mesh(
+    new THREE.CylinderGeometry(treeTrunkWidth, treeTrunkWidth+0.02, treeTrunkHeight),
+    new THREE.MeshLambertMaterial({color: 0x330000})
+    );
+  treeTrunk.position.y = treeTrunkHeight/2;
+  pine[i].add(treeTrunk);
   branch[i] = [];
-  for (var j = 0; j < branchNumber; j++){
-    branch[i][j] = new THREE.Mesh(
-      new THREE.TetrahedronGeometry(),
+  treeLight[i] = [];
+  var outness = 0.3 + Math.random()/3;
+  var pineSlope = Math.random()*3 + 2;
+  for (var j = 0; j < branchNumber; j++){//make branch
+    branch[i][j] = new THREE.Object3D();
+
+
+    //treebranch tetrahedron
+    var tempBranch = new THREE.Mesh(
+      new THREE.TetrahedronGeometry(0.5),
       new THREE.MeshLambertMaterial({color:0x00ee00})
     );
-    branch[i][j].geometry.vertices[0].z = 2;
-    branch[i][j].position.y = j/branchNumber * 10;
-    branch[i][j].scale.set(branchNumber/(j+5) + 1, branchNumber/(j+5) + 1, branchNumber/(j+5) + 1);
-    branch[i][j].rotation.y = j*phi*pi;
+    var tempBranchSlope = -Math.random();
+    tempBranch.geometry.vertices[1].z = 1.5 + outness;
+    tempBranch.geometry.vertices[1].y = tempBranchSlope;
+    branch[i][j].add(tempBranch);
+
+    //ornament
+    if (j > 4){
+      var ornament = new THREE.Mesh(
+        new THREE.DodecahedronGeometry(0.2),
+        new THREE.MeshPhongMaterial({color:0xff0000})
+        );
+      ornament.position.y = -0.2 + tempBranchSlope;
+      ornament.position.z = 1.3 + outness;
+      ornament.position.x = -0.3;
+      branch[i][j].add(ornament);
+
+    // //ornament light TOO SLOOOOOOW :(
+    // treeLight[i][j] = new THREE.PointLight( 0x0000ff, 1, 1);
+    // treeLight[i][j].position = ornament.position;
+    // treeLight[i][j].castShadow = true;
+    // branch[i][j].add( treeLight[i][j] );
+    }
+
+
+
+    branch[i][j].position.y = j/branchNumber * (0.7 + 1.3/(j+1)) + treeTrunkHeight;
+    var branchScale = 2*(branchNumber+1 - j)/(pineSlope*branchNumber+1);
+    branch[i][j].scale.set(branchScale,branchScale,branchScale);
+    branch[i][j].rotation.y = j*phi*tau;
     pine[i].add(branch[i][j]);
   }
-  pine[i].scale.set(0.5,0.5,  0.5);
+  //star
+  var star = new THREE.Object3D();
+  var starPart1= new THREE.Mesh(
+        new THREE.TetrahedronGeometry(),
+        new THREE.MeshBasicMaterial({color:0xffff00})
+        );
+  var starPart2= new THREE.Mesh(
+        new THREE.TetrahedronGeometry(),
+        new THREE.MeshBasicMaterial({color:0xffff00})
+        );
+  starPart2.rotation.y = tau/4;
+  star.add(starPart2,starPart1)
+  star.scale.set(0.1,0.1,0.1);
+  star.rotation.z = tau/8;
+  star.position.y = 0.8 + treeTrunkHeight;
+  pine[i].add(star);
+  //starlights    
+  starlight[i] = new THREE.PointLight( 0xffff00, 1, 5);
+  starlight[i].position = star.position;
+  starlight[i].castShadow = true;
+  pine[i].add( starlight[i] );
+
+  var tempScale = Math.random() + 1;
+  pine[i].scale.set(tempScale,tempScale,tempScale);
   pine[i].position.x = Math.random()*50 - 25;
   pine[i].position.z = Math.random()*50 - 25;
   everything.add(pine[i]);
+}
+
+
+//presents
+var present = [];
+var presentNumber = 20;
+var presentArray = [];
+for (var i = 0; i<presentNumber; i++){
+  presentArray[i] = 0;
+  var presentHeight = 0.1;
+  present[i] = new THREE.Mesh(
+    new THREE.BoxGeometry(0.1 + Math.random()/4, presentHeight, 0.1 + Math.random()/4),
+    new THREE.MeshPhongMaterial()
+    );
+  present[i].material.color.setRGB(2*Math.random(), 2*Math.random(), 2*Math.random());
+  present[i].position.x = Math.random()*50 - 25;
+  present[i].position.z = Math.random()*50 - 25;
+  present[i].position.y = presentHeight/2;
+  everything.add(present[i]);
 }
 
 scene.add(everything);
@@ -185,6 +270,8 @@ scene.add(everything);
   var speed = 0.2;
   var sledToggle = 0;
   var sledToggleDistance = 0.4;
+
+  var presentsFound = 0;
 
   var t = 0;
 /*
@@ -260,10 +347,28 @@ function animate() {
   for (var i = 0; i < pine.length; i++){//make ugly trees jump away from you
     var treePos = new THREE.Vector2();
     treePos.set(pine[i].position.x + everything.position.x, pine[i].position.z + everything.position.z);
-    if (treePos.distanceTo(pos) < 4){
+    if (treePos.distanceTo({x: 0, y: 0}) < 3){
       pine[i].position.x += treePos.x/50;
       pine[i].position.z += treePos.y/50;
     }
+  }
+
+  for (var i = 0; i< presentNumber; i++){
+    var presentPos = new THREE.Vector2();
+    presentPos.set(present[i].position.x + everything.position.x, present[i].position.z + everything.position.z);
+    if (presentPos.distanceTo(pos) < 1){
+      if (presentArray[i] == 0){
+        presentArray[i] = 1;
+        present[i].position.y = presentsFound*presentHeight;
+        presentsFound++
+      }
+      present[i].position.x = -everything.position.x;
+      present[i].position.z = -everything.position.z;
+    }
+  }
+
+  if (presentsFound == presentNumber){
+    pine[0].scale.set(5,5,5);
   }
 
   //Update VR headset position and apply to camera.
